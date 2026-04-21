60c022231729e5c4330110216121a26e794b6605
Dreamer <835831088@qq.com>
2026-04-21 10:49:34 +0800

    feat: 添加axum框架实现HTTP服务器
    将简单的打印程序改为使用axum框架的HTTP服务器，监听8080端口并返回消息
    添加axum和tokio依赖

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,22 @@
-fn main() {
-    println!("Hello CI/CD89fasdf!");
+use std::net::SocketAddr;
+
+use axum::{routing::get, Router};
+
+#[tokio::main]
+async fn main() {
+    // 创建路由
+    let app = Router::new()
+        .route("/", get(|| async { "Hello CI/CD89fasdf!" }));
+
+    // 绑定地址
+    let addr = SocketAddr::from(([0, 0, 0, 0], 8080));
+    println!("Server running on http://{}", addr);
+
+    // 启动服务器
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
 
 #[cfg(test)]
